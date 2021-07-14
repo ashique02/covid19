6a18de13c2dc4a3c1211af9c1cc51200dfb1ae1f
Ashique <ashu0150@gmail.com>
2021-07-14 23:08:45 +0530

    Final Commit

--- resources/js/countries.js
+++ resources/js/countries.js
@@ -213,13 +213,14 @@ const close_list_btn = document.querySelector(".close");
 const input = document.getElementById('search-input')
 
 // CREATE TE COUNTRY LIST
-function createCountryList(){
+function createCountryList() {
     const num_countries = country_list.length;
 
-    let i = 0, ul_list_id;
+    let i = 0,
+        ul_list_id;
 
-    country_list.forEach( (country, index) => {
-        if( index % Math.ceil(num_countries/num_of_ul_lists) == 0){
+    country_list.forEach((country, index) => {
+        if (index % Math.ceil(num_countries / num_of_ul_lists) == 0) {
             ul_list_id = `list-${i}`;
             country_list_element.innerHTML += `<ul id='${ul_list_id}'></ul>`;
             i++;
@@ -237,38 +238,38 @@ let num_of_ul_lists = 3;
 createCountryList();
 
 // SHOW/HIDE THE COUTRY LIST ON CLICK EVENT
-chang_country_btn.addEventListener("click", function(){
+chang_country_btn.addEventListener("click", function() {
     input.value = "";
     resetCountryList();
     search_country_element.classList.toggle("hide");
     search_country_element.classList.add("fadeIn");
 });
 
-close_list_btn.addEventListener("click", function(){
+close_list_btn.addEventListener("click", function() {
     search_country_element.classList.toggle("hide");
 });
 
-country_list_element.addEventListener("click", function(){
+country_list_element.addEventListener("click", function() {
     search_country_element.classList.toggle("hide");
 });
 
 // COUNTRY FILTER
 /* input event fires up whenever the value of the input changes */
-input.addEventListener("input", function(){
+input.addEventListener("input", function() {
     let value = input.value.toUpperCase();
 
-    country_list.forEach( country => {
-        if( country.name.toUpperCase().startsWith(value)){
+    country_list.forEach(country => {
+        if (country.name.toUpperCase().startsWith(value)) {
             document.getElementById(country.name).classList.remove("hide");
-        }else{
+        } else {
             document.getElementById(country.name).classList.add("hide");
         }
     })
 })
 
 // RESET COUNTRY LIST (SHOW ALL THE COUNTRIES )
-function resetCountryList(){
-    country_list.forEach( country => {
+function resetCountryList() {
+    country_list.forEach(country => {
         document.getElementById(country.name).classList.remove("hide");
     })
 }
